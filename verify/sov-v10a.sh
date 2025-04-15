4973bd6b64a0701bbf6797f31d638b221af6d300
Kârūn The Rich <development@xpowermine.com>
2025-04-15 16:42:41 +0300

    xpow: protocol verify

---
+++ verify/sov-v10a.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+source .env
+
+CHAIN_ID=${1-"43114"} # use 43113 (for testnet)
+
+forge verify-contract \
+    --chain-id $CHAIN_ID \
+    --guess-constructor-args \
+    --etherscan-api-key $ETHERSCAN_API_KEY \
+    --compiler-version v0.8.29+commit.ab55807c \
+    --watch \
+    $APOW_SOV_10a \
+    source/contracts/APower.sol:APower
